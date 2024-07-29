b84ace353474626071c61c1d9d04d706f8a4745b
Matteo Di Vita <matteo.divita@outlook.be>
2024-07-29 12:10:04 +0200

    Clean main branch

--- backend/heart/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PredictHeartConditionView, SaveHeartDataView, HeartDataHistoryView
-
-urlpatterns = [
-    path("predict-heart-condition", PredictHeartConditionView.as_view()),
-    path("save-heart-data", SaveHeartDataView.as_view(), name='save-heart-data'),
-    path('history', HeartDataHistoryView.as_view(), name='heart-data-history'),
-
-]
--- backend/lung/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import  predict_lung_image
-
-urlpatterns = [
-    # path("predict-lung", LungPredictAPIView.as_view()),
-    path("predict-lung", predict_lung_image),
-]
-
-
